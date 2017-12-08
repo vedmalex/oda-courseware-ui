d9595d0b93838cc4e37d3021bee459f3f10f3476
vedanta-krit.das <vedmalex@gmail.com>
2017-12-08 16:44:47 +0300

    update changes to Form Edit

--- src/UIoverride/Meeting/queries/index.js
+++ src/UIoverride/Meeting/queries/index.js
@@ -3,48 +3,45 @@ import { fragments, queries } from './queries';
 import set from 'lodash/set';
 
 export default {
-  // queries,
-  // fragments,
-  // name: 'Meeting',
-  // fields: {
-  //   id: { type: 'string' },
-  //   date: { type: 'date' },
-  //   curator: {
-  //     ref: {
-  //       resource: 'Curator',
-  //       type: data.resource.interfaces.refType.BelongsTo,
-  //     },
-  //   },
-  //   group: {
-  //     ref: {
-  //       resource: 'Group',
-  //       type: data.resource.interfaces.refType.BelongsTo,
-  //     },
-  //   },
-  //   students: {
-  //     ref: {
-  //       resource: 'Student',
-  //       type: data.resource.interfaces.refType.BelongsToMany,
-  //     },
-  //   },
-  // },
-  // operations: {
-  //   GET_LIST: {
-  //     _filterBy: (params) => Object.keys(params.filter).reduce((acc, key) => {
-  //       if (key === 'ids') {
-  //         return { ...acc, id: { in: params.filter[key] } };
-  //       }
-  //       if (key === 'q') {
-  //         return { ...acc, id: { imatch: params.filter[key] } };
-  //       }
-  //       return set(acc, key.replace('-', '.'), params.filter[key]);
-  //     }, {}),
-  //   },
-  //   // GET_ONE: {},
-  //   // GET_MANY: {},
-  //   // GET_MANY_REFERENCE: {},
-  //   // CREATE: {},
-  //   // UPDATE: {},
-  //   // DELETE: {},
-  // },
+  fragments: {
+    resultFragment: gql`fragment MeetingResult on Meeting {
+      id
+      date
+
+      curatorId: curator @_(get:"id") {
+        id
+      }
+      groupId: group @_(get:"id") {
+        id
+      }
+      studentsValues: students @_(get:"edges") {
+        edges @_(each: {assign:"node"}) {
+          present
+          specialNotes
+          node  {
+            id
+          }
+        }
+      }
+    }`,
+    fullFragment: gql`fragment MeetingFull on Meeting {
+      id
+      date
+      curator {
+        id
+      }
+      group {
+        id
+      }
+      students {
+        edges {
+          present
+          specialNotes
+          node {
+            id
+          }
+        }
+      }
+    }`,
+  },
 };
--- src/UIoverride/Student/queries.js
+++ src/UIoverride/Student/queries.js
@@ -1,4 +1,6 @@
+import { data } from 'oda-aor-rest';
 import gql from 'graphql-tag';
+
 // fragments
 
 console.log(__filename)
@@ -6,26 +8,9 @@ console.log(__filename)
 export default {
   fields: {
     id: { type: 'string' },
+    // additional field for M:N
     present: { type: 'boolean' },
     specialNotes: { type: 'string' },
-    person: {
-      ref: {
-        resource: 'Person',
-        type: data.resource.interfaces.refType.BelongsTo,
-      },
-    },
-    group: {
-      ref: {
-        resource: 'Group',
-        type: data.resource.interfaces.refType.BelongsTo,
-      },
-    },
-    meetings: {
-      ref: {
-        resource: 'Meeting',
-        type: data.resource.interfaces.refType.BelongsToMany,
-      },
-    },
   },
   fragments: {
     resultFragment: gql`fragment StudentResult on Student {
